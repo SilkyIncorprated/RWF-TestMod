89a318204cf3cc3578cd5116ad38cf47f1ba5821
Silky <lauraacevedo90744@gmail.com>
2024-11-09 18:09:14 -0800

    Add project files.

---
+++ src/Scripts/Character/TestCharacter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RWF.Swagshit;
+
+namespace rwf_testmod.Scripts.Character
+{
+    public class TestCharacter : CharacterScript
+    {
+        public TestCharacter(RWF.Swagshit.Character owner) : base(owner)
+        {
+        }
+    }
+}
---
+++ src/Scripts/Stage/TestStage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RWF.Swagshit;
+
+namespace rwf_testmod.Scripts.Stage
+{
+    public class TestStage : StageScript
+    {
+        public TestStage(RWF.Swagshit.Stage owner) : base(owner)
+        {
+        }
+    }
+}
